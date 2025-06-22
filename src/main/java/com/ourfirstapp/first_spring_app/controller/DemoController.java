4e8d3af726cc7e785c62a08aff71e0d331c54d6f
pranadeep <pogulapranadeep!2@gmail.com>
2025-06-22 20:00:41 +0530

    Dependency injection

---
+++ src/main/java/com/ourfirstapp/first_spring_app/controller/DemoController.java
@@ -0,0 +1,23 @@
+package com.ourfirstapp.first_spring_app.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+//@Controller
+//@ResponseBody
+@RestController
+@RequestMapping("/demo/apis")
+public class DemoController{
+    @GetMapping("/demo1")
+    public String getDemo(){
+        System.out.println("This is our first Demo API");
+        return "This is our first Demo API";
+    }
+    @GetMapping("/demo2")
+    public String getDemo2(){
+        System.out.println("This is our second Demo API");
+        return "This is our second Demo API";
+    }
+
+}
